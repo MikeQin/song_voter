1d0ff4ed0d3106ecf2f718dd779ed2ec86784d9d
MikeQin <mike.qin@gmail.com>
2019-11-07 02:55:08 -0500

    init

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:song_voter/models/song.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
